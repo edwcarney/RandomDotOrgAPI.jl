fee4a1ce8b30307b2774ea1e1cbd93654167a9b0
Edward Carney <carne006@gmail.com>
2024-05-10 12:36:39 -0700

    Update RandomDotOrgAPI.jl

--- src/RandomDotOrgAPI.jl
+++ src/RandomDotOrgAPI.jl
@@ -30,6 +30,7 @@ apiKeyFile = string(pwd(),"/","myapikey.jl")
 myapikey = "00000000-0000-0000-0000-000000000000"
 if isfile(apiKeyFile)
     include(apiKeyFile)
+    println(string("API key = ", myapikey))
 end
 
 export  get_usage, check_usage, get_result, verify_signature, generate_integers, generate_integer_sequences, generate_strings, generate_gaussians,
